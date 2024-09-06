4655a8acc1fe873f7d8958308bd981240e2e9098
3O1 <james@meetz.so>
2024-09-06 17:22:33 -0400

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h2 className="text-black font-bold text-2xl">coming soon</h2>
+    </div>
+  );
 }
