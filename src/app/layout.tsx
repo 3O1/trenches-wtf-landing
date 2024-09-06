cfb9361eb3ccc7b83c5f0b346b6d846c00f61b0e
3O1 <james@meetz.so>
2024-09-06 17:42:47 -0400

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="TRENCHES.WTF - BUY, SELL, SHITPOST" />
+
+        <meta
+          property="og:title"
+          content="TRENCHES.WTF - BUY, SELL, SHITPOST"
+        />
+        <meta
+          property="og:description"
+          content="Solana's underground shitcoin forum."
+        />
+        <meta
+          property="og:image"
+          content="https://trenches-wtf.s3.amazonaws.com/trenches-logo.png"
+        />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="720" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@trenchesdotwtf" />
+        <meta name="twitter:title" content="trenches" />
+        <meta
+          name="twitter:description"
+          content="Solana's underground shitcoin forum."
+        />
+        <meta
+          name="twitter:image"
+          content="https://trenches-wtf.s3.amazonaws.com/trenches-logo.png"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
