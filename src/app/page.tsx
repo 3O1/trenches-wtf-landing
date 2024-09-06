19c57364923f16c4f59de6fea27e438522d5aa7d
3O1 <james@meetz.so>
2024-09-06 17:35:00 -0400

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <h2 className="text-black font-bold text-2xl">coming soon</h2>
+      <h2 className="text-black font-bold text-2xl">
+        this is our landing page
+      </h2>
     </div>
   );
 }
